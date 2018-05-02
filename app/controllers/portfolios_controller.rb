629365ee9f908c11780bacf06b1a7378b1d49af4
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-05-02 21:57:07 +1000

    add basic parse JSON

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,7 @@
 class PortfoliosController < ApplicationController
   def show
+  	require 'json'
+  	require 'open-uri'
   	@user = current_user
   	@portfolio = @user.portfolios.first
   end
--- app/models/share.rb
+++ app/models/share.rb
@@ -1,4 +1,13 @@
 class Share < ApplicationRecord
   belongs_to :portfolio
   # belongs_to :user through :portfolio
+
+  def get_api_data
+  	url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=#{self.ticker}&interval=1min&apikey=6NMSD98HUMQLYIDA"
+		data_serialized = open(url).read
+		data = JSON.parse(data_serialized)
+		data
+		self.ticker
+  end	
+
 end
