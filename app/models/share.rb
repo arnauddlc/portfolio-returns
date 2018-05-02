08f90a20e6065283196d501d43619688db35e81f
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-05-02 22:00:00 +1000

    add api key

--- app/models/share.rb
+++ app/models/share.rb
@@ -3,7 +3,7 @@ class Share < ApplicationRecord
   # belongs_to :user through :portfolio
 
   def get_api_data
-  	url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=#{self.ticker}&interval=1min&apikey=6NMSD98HUMQLYIDA"
+  	url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=#{self.ticker}&interval=1min&apikey=#{ENV['ALPHA_VANTAGE_API_KEY']}"
 		data_serialized = open(url).read
 		data = JSON.parse(data_serialized)
 		data
